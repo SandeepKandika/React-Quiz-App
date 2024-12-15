ef1ba63a5c7d77b81f17b8aed388e73a8b0c4ead
sandeep kandika <108345350+SandeepKandika@users.noreply.github.com>
2024-12-15 07:54:04 +0530

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './resources/styles.css';
-
 import Header from './components/header_footer/Header.jsx';
 import Main from './components/core/Main.jsx';
 import Footer from './components/header_footer/Footer.jsx';
